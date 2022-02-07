6d9d18bf5c69f3a71ab0217f6ae8ce97e4cfb80f
CyberHunter <pramodkrv96@gmail.com>
2022-02-07 11:12:15 -0500

    try commit

---
+++ react/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    fetch('/people').then(response => response.json().then(data => {
+      console.log(data);
+      document.getElementById('root').innerHTML
+        = " <p>First Name: ".concat(data.firstname, "</p>\n            <p>Last Name: ").concat(data.lastname, "</p>\n            <p>Age: ").concat(data.age, "</p>\n            <p>Worked at:</p>\n            <ol>\n                <li>").concat(data.companies[0], "</li>\n                <li>").concat(data.companies[1], "</li>\n            </ol>");
+    }));
+  },[])
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+
+    </div>
+  );
+}
+
+export default App;
